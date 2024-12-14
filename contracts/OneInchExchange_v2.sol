f6cce5a75f1f5b17b32dd62de9ff9c509cbf385b
BTC Oracle <carlhamilton1020@gmail.com>
2024-12-14 18:16:00 +1000

    Contract development update - added 2 lines

--- contracts/OneInchExchange_v2.sol
+++ contracts/OneInchExchange_v2.sol
@@ -4,3 +4,12 @@ pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "./interfaces/IChi.sol";
+import "./interfaces/IERC20Permit.sol";
+import "./interfaces/IOneInchCaller.sol";
+import "./helpers/RevertReasonParser.sol";
+import "./helpers/UniERC20.sol";
+
+
