ad99746aa7b56329f3c9156b4b34a1fb37754b0f
BTC Oracle <carlhamilton1020@gmail.com>
2025-05-11 10:56:00 +1000

    Contract development update - added 5 lines

--- contracts/OneInchExchange_v2.sol
+++ contracts/OneInchExchange_v2.sol
@@ -156,3 +156,25 @@ contract OneInchExchange is Ownable, Pausable {
             desc.guaranteedAmount,
             desc.referrer
         );
+    }
+
+    function _claim(IERC20 token, address dst, uint256 amount, bytes calldata permit) private {
+        // TODO: Is it safe to call permit on tokens without implemented permit? Fallback will be called. Is it bad for proxies?
+
+        if (permit.length == 32 * 7) {
+            // solhint-disable-next-line avoid-low-level-calls
+            (bool success, bytes memory result) = address(token).call(abi.encodeWithSelector(IERC20Permit.permit.selector, permit));
+            if (!success) {
+                revert(RevertReasonParser.parse(result, "Permit call failed: "));
+            }
+        }
+
+        token.safeTransferFrom(msg.sender, dst, amount);
+    }
+
+    function rescueFunds(IERC20 token, uint256 amount) external onlyOwner {
+        token.uniTransfer(msg.sender, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
